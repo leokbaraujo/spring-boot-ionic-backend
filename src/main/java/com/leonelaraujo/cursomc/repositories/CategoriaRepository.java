108182723aef3f3616e7b23a5d59779cc76e624e
Leonel Araujo <leonelaraujo3.0@gmail.com>
2018-10-08 22:15:23 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/leonelaraujo/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.leonelaraujo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.leonelaraujo.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
