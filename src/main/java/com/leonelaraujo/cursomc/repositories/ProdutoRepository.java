ed620404b48144df29fa84fca26659f966a7fd79
Leonel Araujo <leonelaraujo3.0@gmail.com>
2018-10-10 18:11:42 -0300

    Produtos e associacao muitos para muitos

---
+++ src/main/java/com/leonelaraujo/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.leonelaraujo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.leonelaraujo.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
