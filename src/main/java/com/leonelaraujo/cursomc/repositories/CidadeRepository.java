c92f631fea472fdf20e820d98f7d85a206ef67c3
Leonel Araujo <leonelaraujo3.0@gmail.com>
2018-10-12 11:39:08 -0300

    Criacao das entidades Estado e Cidade

---
+++ src/main/java/com/leonelaraujo/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.leonelaraujo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.leonelaraujo.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
