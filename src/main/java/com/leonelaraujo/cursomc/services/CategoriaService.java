a8824f5b9358bf02a2f80ea68d18836e4a488320
Leonel Araujo <leonelaraujo3.0@gmail.com>
2018-10-25 19:47:24 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/leonelaraujo/cursomc/services/CategoriaService.java
+++ src/main/java/com/leonelaraujo/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.leonelaraujo.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,11 @@ public class CategoriaService {
 				));
 				
 	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
+	
 	
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
